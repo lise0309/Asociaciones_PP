47dc7fc5dfbf9aa1d521a2dfbc9cb6d55ef0fb65
Arthur072910 <pocasangrerene729gmail.com>
2026-05-18 08:12:08 -0600

    comentarios

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -138,14 +138,14 @@ if ($rol === 'admin') {
             Reportes y analytics
         </a>
 
-        <div class="sidebar-section-label">Sistema</div>
+  <!--       <div class="sidebar-section-label">Sistema</div>
 
         <a href="configuracion.php" class="sidebar-link <?= $pagina === 'configuracion.php' ? 'active' : '' ?>">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M7.84 1.804A1 1 0 018.82 1h2.36a1 1 0 01.98.804l.331 1.652a6.993 6.993 0 011.929 1.115l1.598-.54a1 1 0 011.186.447l1.18 2.044a1 1 0 01-.205 1.251l-1.267 1.113a7.047 7.047 0 010 2.228l1.267 1.113a1 1 0 01.206 1.25l-1.18 2.045a1 1 0 01-1.187.447l-1.598-.54a6.993 6.993 0 01-1.929 1.115l-.33 1.652a1 1 0 01-.98.804H8.82a1 1 0 01-.98-.804l-.331-1.652a6.993 6.993 0 01-1.929-1.115l-1.598.54a1 1 0 01-1.186-.447l-1.18-2.044a1 1 0 01.205-1.251l1.267-1.114a7.05 7.05 0 010-2.227L1.821 7.773a1 1 0 01-.206-1.25l1.18-2.045a1 1 0 011.187-.447l1.598.54A6.993 6.993 0 017.51 3.456l.33-1.652zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd"/>
             </svg>
             Configuración
-        </a>
+        </a> -->
 
         <?php else: ?>
         <!-- ════ MENÚ VENDEDOR ════ -->
@@ -177,14 +177,14 @@ if ($rol === 'admin') {
 
         <div class="sidebar-section-label">Clientes</div>
 
-        <a href="consultas.php" class="sidebar-link <?= $pagina === 'consultas.php' ? 'active' : '' ?>">
+       <!--  <a href="consultas.php" class="sidebar-link <?= $pagina === 'consultas.php' ? 'active' : '' ?>">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M10 2c-2.236 0-4.43.18-6.57.524C1.993 2.755 1 4.014 1 5.426v5.148c0 1.413.993 2.67 2.43 2.902.848.137 1.705.248 2.57.331v3.443a.75.75 0 001.28.53l3.58-3.579a.78.78 0 01.527-.224 41.202 41.202 0 005.183-.5c1.437-.232 2.43-1.49 2.43-2.903V5.426c0-1.413-.993-2.67-2.43-2.902A41.289 41.289 0 0010 2zm0 7a1 1 0 100-2 1 1 0 000 2zM6 9a1 1 0 11-2 0 1 1 0 012 0zm5 1a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"/>
             </svg>
             Consultas
             <span class="sidebar-badge sidebar-badge-gold"><?= $totalConsultas ?></span>
-        </a>
-
+        </a>-->
+        
         <div class="sidebar-section-label">Documentos</div>
 
         <a href="contratos.php" class="sidebar-link <?= $pagina === 'contratos.php' ? 'active' : '' ?>">
